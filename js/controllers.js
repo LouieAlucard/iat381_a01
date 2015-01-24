afdc139cf74c05f2230e43f761a0218619ee33d4
LouieAlucard <llouieliuye@hotmail.com>
2015-01-23 18:10:06 -0800

    array changed

--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,11 @@
 var recycleControllers = angular.module('recycleControllers', []);
 
 recycleControllers.controller('RecycleListCtrl', function ($scope) {
-	$scope.recycleList = ['Paper', 'Food', 'Container', 'Composite'];
-	$scope.paperList = ['newspaper', 'sketch paper'];
-	$scope.foodList = ['meat', 'vegitable'];
-	$scope.containerList = ['can', 'bottle'];
-	$scope.compositeList = ['trash', 'other'];
+	$scope.recycleList = {
+		['name': 'Paper'], 
+		['name': 'Food'], 
+		['name': 'Container'], 
+		['name': 'Composite'];
 });
 
 
