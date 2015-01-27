6c1602587d237853ea23c4f27a0db0be43bab30c
LouieAlucard <llouieliuye@hotmail.com>
2015-01-26 17:19:28 -0800

    no message

--- js/controllers.js
+++ js/controllers.js
@@ -14,9 +14,9 @@ recycleControllers.controller('CategoryItemCtrl', ['$scope', '$routeParams',
 		$scope.category = $routeParams.category;
 		
 		$scope.categoryItem = [
-		{'category':'Paper', 'item':['news','cardboard']}, 
-		{'category':'Food', 'item':'meat'}, 
-		{'category':'Container', 'item':'can'}, 
-		{'category':'Composite', 'item':'trash'}];
+		{'cg':'Paper', 'item':['news','cardboard']}, 
+		{'cg':'Food', 'item':'meat'}, 
+		{'cg':'Container', 'item':'can'}, 
+		{'cg':'Composite', 'item':'trash'}];
 	}
 ]);
